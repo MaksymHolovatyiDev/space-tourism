0d3a9f6e1648ccaecd5ef588f57e78118d872f5f
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-10-18 17:40:44 +0300

    Add images

--- src/entities/crew-member/lib/index.ts
+++ src/entities/crew-member/lib/index.ts
@@ -1,30 +1,35 @@
+import douglas from 'entities/crew-member/assets/image-douglas-hurley.webp';
+import mark from 'entities/crew-member/assets/image-mark-shuttleworth.webp';
+import victor from 'entities/crew-member/assets/image-victor-glover.webp';
+import anousheh from 'entities/crew-member/assets/image-anousheh-ansari.webp';
+
 export const crew = [
   {
     name: 'Douglas Hurley',
     role: 'Commander ',
     bio: 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.',
-    img: 'src/entities/crew-member/assets/image-douglas-hurley.webp',
+    img: douglas,
     imgRight: 137,
   },
   {
     name: 'MARK SHUTTLEWORTH',
     role: 'Mission Specialist ',
     bio: 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.',
-    img: 'src/entities/crew-member/assets/image-mark-shuttleworth.webp',
+    img: mark,
     imgRight: 186.56,
   },
   {
     name: 'Victor Glover',
     role: 'PILOT',
     bio: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ',
-    img: 'src/entities/crew-member/assets/image-victor-glover.webp',
+    img: victor,
     imgRight: 142.61,
   },
   {
     name: 'Anousheh Ansari',
     role: 'Flight Engineer',
     bio: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. ',
-    img: 'src/entities/crew-member/assets/image-anousheh-ansari.webp',
+    img: anousheh,
     imgRight: 89.43,
   },
 ];
--- src/entities/technology-item/lib/index.ts
+++ src/entities/technology-item/lib/index.ts
@@ -1,23 +1,27 @@
+import vehiclePortrait from 'entities/technology-item/assets/image-launch-vehicle-portrait.jpg';
+import vehicleLandscape from 'entities/technology-item/assets/image-launch-vehicle-landscape.jpg';
+import capsulePortrait from 'entities/technology-item/assets/image-space-capsule-portrait.jpg';
+import capsuleLandscape from 'entities/technology-item/assets/image-space-capsule-landscape.jpg';
+import spaceportPortrait from 'entities/technology-item/assets/image-spaceport-portrait.jpg';
+import spaceportLandscape from 'entities/technology-item/assets/image-spaceport-landscape.jpg';
+
 export const technologies = [
   {
     technology: 'LAUNCH VEHICLE',
     desc: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
-    img: 'src/entities/technology-item/assets/image-launch-vehicle-portrait.jpg',
-    imgLandscape:
-      'src/entities/technology-item/assets/image-launch-vehicle-landscape.jpg',
+    img: vehiclePortrait,
+    imgLandscape: vehicleLandscape,
   },
   {
     technology: 'SPACEPORT',
     desc: 'A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.',
-    img: 'src/entities/technology-item/assets/image-space-capsule-portrait.jpg',
-    imgLandscape:
-      'src/entities/technology-item/assets/image-space-capsule-landscape.jpg',
+    img: capsulePortrait,
+    imgLandscape: capsuleLandscape,
   },
   {
     technology: 'SPACE CAPSULE',
     desc: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
-    img: 'src/entities/technology-item/assets/image-spaceport-portrait.jpg',
-    imgLandscape:
-      'src/entities/technology-item/assets/image-spaceport-landscape.jpg',
+    img: spaceportPortrait,
+    imgLandscape: spaceportLandscape,
   },
 ];
